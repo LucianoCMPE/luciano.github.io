0d6e7c7abb8a751e3de44353ff4a3da2d08091a4
Luciano M <32829781+LucianoMigliaccio@users.noreply.github.com>
2017-11-10 12:40:23 -0500

    Create lucianosweather_jsfunctions-v0.1.js

---
+++ lucianosweather_jsfunctions-v0.1.js
@@ -0,0 +1,14 @@
+$(function(){
+
+  var d = new Date();
+  var weekday = new Array(7);
+	weekday[6] = "Sunday";
+	weekday[0] = "Monday";
+	weekday[1] = "Tuesday";
+	weekday[2] = "Wednesday";
+	weekday[3] = "Thursday";
+	weekday[4] = "Friday";
+  weekday[5] = "Saturday";
+  var n = weekday[d.getDay()];
+	$("#day1").text(n);
+});
