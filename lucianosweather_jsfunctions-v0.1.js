b945ac0f511e777d449b04c39696de3b237784b8
Luciano M <32829781+LucianoMigliaccio@users.noreply.github.com>
2017-11-11 01:06:05 -0500

    Update lucianosweather_jsfunctions-v0.1.js

--- lucianosweather_jsfunctions-v0.1.js
+++ lucianosweather_jsfunctions-v0.1.js
@@ -28,6 +28,58 @@ $(function(){
 	$("#day2").text(c);
 });
 
+$(function(){
+
+  var ee = new Date();
+  var weekday = new Array(7);
+	weekday[4] = "Sunday";
+	weekday[5] = "Monday";
+	weekday[6] = "Tuesday";
+	weekday[0] = "Wednesday";
+	weekday[1] = "Thursday";
+	weekday[2] = "Friday";
+	weekday[3] = "Saturday";
+  var cc = weekday[ee.getDay()];
+	$("#day3").text(cc);
+});
+
+  var d = new Date();
+  var weekday = new Array(7);
+	weekday[3] = "Sunday";
+	weekday[4] = "Monday";
+	weekday[5] = "Tuesday";
+	weekday[6] = "Wednesday";
+	weekday[0] = "Thursday";
+	weekday[1] = "Friday";
+	weekday[2] = "Saturday";
+  var n = weekday[d.getDay()];
+	$("#day4").text(n);
+});
+  var d = new Date();
+  var weekday = new Array(7);
+	weekday[2] = "Sunday";
+	weekday[3] = "Monday";
+	weekday[4] = "Tuesday";
+	weekday[5] = "Wednesday";
+	weekday[6] = "Thursday";
+	weekday[0] = "Friday";
+	weekday[1] = "Saturday";
+  var n = weekday[d.getDay()];
+	$("#day5").text(n);
+});
+
+var d = new Date();
+  var weekday = new Array(7);
+	weekday[1] = "Sunday";
+	weekday[2] = "Monday";
+	weekday[3] = "Tuesday";
+	weekday[4] = "Wednesday";
+	weekday[5] = "Thursday";
+	weekday[6] = "Friday";
+	weekday[0] = "Saturday";
+  var n = weekday[d.getDay()];
+	$("#day6").text(n);
+});
 
 
 
