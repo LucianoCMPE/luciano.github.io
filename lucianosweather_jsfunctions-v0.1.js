4dcd5797297ca3781fea5aef8344fbc2ae6c9ccb
Luciano M <32829781+LucianoMigliaccio@users.noreply.github.com>
2017-11-15 12:03:54 -0500

    Update lucianosweather_jsfunctions-v0.1.js

--- lucianosweather_jsfunctions-v0.1.js
+++ lucianosweather_jsfunctions-v0.1.js
@@ -15,7 +15,8 @@ $(function(){
 	// Change this to your API key between the single quotes ('):
 	var api_key = '924dbd1a7a384a1d6dfb73f3a1319631';
 	var weather_data = loadjson('http://api.openweathermap.org/data/2.5/weather?q=Baltimore,ca&units=imperial&appid=' + api_key);
-
+	alert(weather_data);
+	alert(weather_data.main.temp);
 	$("#temp1").text(weather_data.main.temp);
 });
 
