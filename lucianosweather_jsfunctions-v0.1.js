a82676811120f8a5b03c1494cf888c96ea814c0c
Luciano M <32829781+LucianoMigliaccio@users.noreply.github.com>
2017-11-15 12:47:25 -0500

    Update lucianosweather_jsfunctions-v0.1.js

--- lucianosweather_jsfunctions-v0.1.js
+++ lucianosweather_jsfunctions-v0.1.js
@@ -24,7 +24,7 @@ $(function(){
 	//}
 	$.getJSON('http://api.openweathermap.org/data/2.5/weather?q=Baltimore,us&units=imperial&appid=924dbd1a7a384a1d6dfb73f3a1319631', function(data) {
     //data is the JSON string
-		$("#temp1").html(data.main.temp + '<sup>o</sup>');
+		$("#temp1").html(Math.round(data.main.temp) + '<sup>o</sup>');
 });
 	//alert(weather_data);
 	//alert(weather_data.main.temp);
