a55a9a8f0e93ddfa12410e743270d0093fd27b67
Luciano M <32829781+LucianoMigliaccio@users.noreply.github.com>
2017-11-16 11:29:24 -0500

    Update lucianosweather_jsfunctions-v0.1.js

--- lucianosweather_jsfunctions-v0.1.js
+++ lucianosweather_jsfunctions-v0.1.js
@@ -104,6 +104,13 @@ $(function(){
 	$("#day6").text(s);
 });
 
-
+$(document).ready(function(){  
+  $("#btn1").click(function(){  
+    $("div").fadeOut(3000);  
+  });  
+   $("#btn2").click(function(){  
+    $("div").fadeIn(3000);  
+  });   
+});  
 
 
