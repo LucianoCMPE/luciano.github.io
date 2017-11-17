2a2251462e5965977865f4df3cb50a43d12a037e
Luciano M <32829781+LucianoMigliaccio@users.noreply.github.com>
2017-11-17 10:53:31 -0500

    Update lucianosweather_jsfunctions-v0.1.js

--- lucianosweather_jsfunctions-v0.1.js
+++ lucianosweather_jsfunctions-v0.1.js
@@ -1,4 +1,4 @@
-	$("#table").show("slow").css("display","table");
+	$("#table").hide().show("slow").css("display","table");
 	$("#date").fadeIn(1000).css("display","inline-block");
 	$(function(){
 	var d = new Date();
